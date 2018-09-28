076dc109f826ebe7c32600e652c3283f184ec208
alexisbrabo <alexis_brabo@hotmail.com>
2018-09-28 09:25:56 -0300

    Aula 3.09

--- src/main/java/com/alexisbrabo/almoney/api/model/Categoria.java
+++ src/main/java/com/alexisbrabo/almoney/api/model/Categoria.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "categoria")
@@ -13,6 +14,9 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
+	private String nome;
 	
 	public Long getCodigo() {
 		return codigo;
@@ -55,5 +59,5 @@ public class Categoria {
 		this.nome = nome;
 	}
 
-	private String nome;
+	
 }
