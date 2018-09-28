c55bda9fbca035db59965da70a2ed6d8b9748af7
alexisbrabo <alexis_brabo@hotmail.com>
2018-09-28 17:04:44 -0300

    Aula 3.10

--- src/main/java/com/alexisbrabo/almoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/alexisbrabo/almoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.alexisbrabo.almoney.api.exceptionHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -24,14 +30,31 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause().toString();
- 
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-	
-		return handleExceptionInternal(ex, "", headers, HttpStatus.BAD_REQUEST, request);
+
+		List<Erro> erros = criarListaDeErro(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	private List<Erro> criarListaDeErro(BindingResult bindingResult) {
+
+		List<Erro> erros = new ArrayList<>();
+
+		for (FieldError error : bindingResult.getFieldErrors()) {
+
+			String mensagemUsuario = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = error.toString();
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+
+		return erros;
 	}
 
 	public static class Erro {
@@ -39,7 +62,6 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		private String mensagemDesenvolvedor;
 
 		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
-			super();
 			this.mensagemUsuario = mensagemUsuario;
 			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
 		}
