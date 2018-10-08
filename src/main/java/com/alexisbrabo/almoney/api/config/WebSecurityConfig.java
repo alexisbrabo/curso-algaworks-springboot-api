4dbab165a37818a5134bce6185d391a2542843a7
alexisbrabo <alexis_brabo@hotmail.com>
2018-10-08 15:12:07 -0300

    Aua 6.3

---
+++ src/main/java/com/alexisbrabo/almoney/api/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.alexisbrabo.almoney.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+}
