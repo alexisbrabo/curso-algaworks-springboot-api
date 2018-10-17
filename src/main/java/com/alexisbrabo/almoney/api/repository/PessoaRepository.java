93b620cb77450497b792a246a433287941843b57
alexisbrabo <alexis_brabo@hotmail.com>
2018-10-17 09:51:38 -0300

    Aula 7.7

--- src/main/java/com/alexisbrabo/almoney/api/repository/PessoaRepository.java
+++ src/main/java/com/alexisbrabo/almoney/api/repository/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.alexisbrabo.almoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alexisbrabo.almoney.api.model.Pessoa;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
