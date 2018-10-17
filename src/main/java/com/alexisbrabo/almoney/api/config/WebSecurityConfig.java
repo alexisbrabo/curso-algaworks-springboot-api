20b6e0a826e212475fa46c83e65f1d208ee5ae68
alexisbrabo <alexis_brabo@hotmail.com>
2018-10-17 08:43:18 -0300

    Aula 7.6

--- src/main/java/com/alexisbrabo/almoney/api/config/WebSecurityConfig.java
+++ src/main/java/com/alexisbrabo/almoney/api/config/WebSecurityConfig.java
@@ -2,11 +2,13 @@ package com.alexisbrabo.almoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Profile("oauth-security")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
